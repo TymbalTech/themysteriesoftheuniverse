15a3c6259f5d96325bda519b9c5b31b783a16485
TymbalTech <nan@tymbaltech.com>
2024-07-04 17:08:23 -0500

    Added basic HTML structure, Meyer's reset CSS, and a basic JavaScript file

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// This script will run once the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Welcome to The Mysteries of the Universe!");
+});
