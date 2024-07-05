02e6e06deb6874e8aa755f433361f6773722961f
TymbalTech <nan@tymbaltech.com>
2024-07-05 18:21:41 -0500

    Added console log for publish date and time to script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Welcome to The Mysteries of the Universe!");
 });
+
+// Console log for publish date and time
+console.log(
+  `Published on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`
+);
